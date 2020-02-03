0131f0fdeba6b296557335cd90155af572ebadcb
Catalin <lungancatalin@yahoo.com>
2020-02-03 21:29:46 +0200

    Updates

---
+++ precache-manifest.01b9b1e942bc9553061e283cd0f1b14f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b199b27b936f3e62853f98523d2675",
+    "url": "/order_list/index.html"
+  },
+  {
+    "revision": "938d3b396ccaf1a02665",
+    "url": "/order_list/static/css/main.a04d25d9.chunk.css"
+  },
+  {
+    "revision": "632b5984a4325bdfa217",
+    "url": "/order_list/static/js/2.a56c8ee0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/order_list/static/js/2.a56c8ee0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "938d3b396ccaf1a02665",
+    "url": "/order_list/static/js/main.14321ca0.chunk.js"
+  },
+  {
+    "revision": "0be7d6358e899dabc2d5",
+    "url": "/order_list/static/js/runtime-main.25af32a0.js"
+  }
+]);
